8a2f8282bb7959dcbba2b7a21ee7bbd8780a81af
Isabella Leung <isabellaleung56@gmail.com>
2018-10-21 23:46:07 +1000

    Started ReadNameFile class

--- NameSorter/NameSorter/ReadNameFile.cs
+++ NameSorter/NameSorter/ReadNameFile.cs
@@ -6,6 +6,16 @@ namespace NameSorter
 {
     class ReadNameFile
     {
+        private string _filePath;
 
+        public ReadNameFile(string filePath)
+        {
+            filePath = _filePath;
+        }
+
+        public void ReadGivenFile()
+        {
+            string text = 
+        }
     }
 }
